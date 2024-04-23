90c802a585905906fc1157e0202aea574f7e43d6
Juanfe <juanfe.ttv@outlook.com>
2024-04-23 09:26:59 -0500

    Ch17-Ch18: Usando el mismo test para validar cada sección y sus títulos

--- tests/seccion4/ch15-validar-titulo.spec.ts
+++ tests/seccion4/ch15-validar-titulo.spec.ts
@@ -1,4 +1,8 @@
-// Feature: validar titulo de una web
+/** 
+ * Feature: validar titulo de una web
+ * Estas validaciones estan usando trabajando en un localizador que se 
+ * encuentre en el header! page.locator('#page_header')
+ */
 import { test, Browser, type Page, expect } from '@playwright/test';
 
 (async () => {
@@ -6,24 +10,35 @@ import { test, Browser, type Page, expect } from '@playwright/test';
     let page: Page;
 
     test.describe('Navegacion en https://www.freerangetesters.com/', () => {
-        test('El link de base carga correctamente', async ({ page }) => {
-            await test.step('Estando yo en la web principal de FreeRangeTesters', async () => {
-                await page.goto("https://www.freerangetesters.com/");
-                //Validacion del nombre de la pestaña
-                await expect(page).toHaveTitle("Free Range Testers");
-            })
 
-            await test.step('Cuando hago click en "Cursos"', async () => {
-                page.locator('#page_header').getByRole('link', { name: 'Cursos', exact: true }).click();
-                //Esperamos que la url de la web termine con el path indicado
-                await page.waitForURL('**/cursos')
-            })
+        const secciones = [
+            { nombre: 'Cursos', url: '/cursos', tituloEsperado: 'Cursos' },
+            { nombre: 'Udemy', url: '/udemy', tituloEsperado: 'Udemy' },
+            { nombre: 'Recursos', url: '/recursos', tituloEsperado: 'Recursos' }
+            //Agregar mas secciones si es necesario
+        ];
 
-            await test.step('Soy redirigido a la seccion de titulo "Cursos"', async  () => {
-                //A Expect le entregamos el contexto para la validacion, en este caso la pagina
-                await expect(page).toHaveTitle("Cursos");
-            })
-        })
+        //Este for nos permite iterar en las diferentes paginas que esta en el arreglo 'secciones'
+        for (const seccion of secciones) {
+             test(`Validar redireccion a la seccion "${seccion.nombre}"`, async ({ page }) => {
 
+                await test.step('Estando yo en la web principal de FreeRangeTesters', async () => {
+                    await page.goto("https://www.freerangetesters.com/");
+                    //Validacion del nombre de la pestaña
+                    await expect(page).toHaveTitle("Free Range Testers");
+                })
+
+                await test.step(`Cuando hago click en "${seccion.nombre}"`, async () => {
+                    page.locator('#page_header').getByRole('link', { name: seccion.nombre, exact: true }).click();
+                    //Esperamos que la url de la web termine con el path indicado
+                    await page.waitForURL(`**${seccion.url}`)
+                })
+
+                await test.step(`Soy redirigido a la seccion de titulo "${seccion.tituloEsperado}"`, async () => {
+                    //A Expect le entregamos el contexto para la validacion, en este caso la pagina
+                    await expect(page).toHaveTitle(seccion.tituloEsperado);
+                })
+            })
+        }
     })
 })();
